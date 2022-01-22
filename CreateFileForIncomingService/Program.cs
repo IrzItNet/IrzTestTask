e67eebc7abff9c4da0fa2fae2200220f5dd096ab
olegtutaev <63103153+olegtutaev@users.noreply.github.com>
2022-01-22 17:37:05 +0400

    UPD: Program.cs: Console monitoring XML - 200/400

--- CreateFileForIncomingService/Program.cs
+++ CreateFileForIncomingService/Program.cs
@@ -30,9 +30,46 @@ namespace CreateFileForIncomingService
                     Document.СчетФактураВыданныйАванс,
                 },
             };
+        
+        public static string postXMLData(string destinationUrl, string xml)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(destinationUrl);
+            byte[] bytes;
+            bytes = System.Text.Encoding.ASCII.GetBytes(xml);
+            request.ContentType = "text/xml; encoding='utf-8'";
+            request.ContentLength = bytes.Length;
+            request.Method = "POST";
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+            HttpWebResponse response;
+            response = (HttpWebResponse)request.GetResponse(); // В случае 400 в консоли выдаст System.Net.WebException
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream responseStream = response.GetResponseStream();
+                string responseStr = new StreamReader(responseStream).ReadToEnd();
+                Console.WriteLine("200 OK");
+                return responseStr;
+            }
+            else
+                return null;
+        }
 
         static void Main()
         {
+            List<string> list = new List<string>();
+
+            foreach (var file in Directory.GetFiles(@"..//..//..//..//IrzTestTask-main\IrzTestFile\", "*.xml", SearchOption.AllDirectories))
+                list.Add(file);
+
+            for (int i=0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " returned: ");
+                var xml = XDocument.Load(list[i]);
+                postXMLData("http://localhost:63524//api/Incoming", xml.ToString());
+            }
+            
             var path = ConfigurationManager.AppSettings["PathForFilesIncomingService"];
             if(Directory.Exists(path))
                 Directory.Delete(path, true);
